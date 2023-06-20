d2e7e80d7e3a005be3ba2232680a5d0b069c6fa0
junkor-1011 <tomoyukizxy@gmail.com>
2023-06-20 23:51:17 +0900

    chore: eslint & prettier setting

---
+++ .eslintrc.base.cjs
@@ -0,0 +1,23 @@
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+});
