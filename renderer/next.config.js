bd3c2c0343ade53498fb40de340ac4c42bf48888
junkor-1011 <tomoyukizxy@gmail.com>
2023-06-23 03:24:11 +0900

    vanilla-extract

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,6 +1,12 @@
+// @ts-check
+
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
 };
 
-module.exports = nextConfig;
+module.exports = withVanillaExtract(nextConfig);
--- renderer/src/app/next/page.tsx
+++ renderer/src/app/next/page.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import SendButton from './SendButton';
 import InvokeButton from './InvokeButton';
 import styles from './page.module.css';
+import { exampleStyle } from './style.css';
 
 const Home: React.FC = () => {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
         <a href="/" className={styles.card}>
-          <h2>return to home</h2>
+          <h2 className={exampleStyle}>return to home</h2>
         </a>
         <SendButton />
         <InvokeButton />
